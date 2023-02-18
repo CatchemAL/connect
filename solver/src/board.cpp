48861daf9a4579ed98345ae9f5279093610b5b87
Alex Cross <alexjcross90@gmail.com>
2023-02-18 13:17:54 +0000

    is win method

--- solver/src/board.cpp
+++ solver/src/board.cpp
@@ -20,7 +20,20 @@ namespace connect {
 		}
 
 		bool Board::is_win() {
-			return true;
+
+			auto bitboard = position;
+			uint8_t h1 = HEIGHT + 0, h2 = HEIGHT + 1, h3 = HEIGHT + 2, h4 = 1;
+
+			if (bitboard & (bitboard >> h1) & (bitboard >> 2 * h1) & (bitboard >> 3 * h1))
+				return true;
+			if (bitboard & (bitboard >> h2) & (bitboard >> 2 * h2) & (bitboard >> 3 * h2))
+				return true;
+			if (bitboard & (bitboard >> h3) & (bitboard >> 2 * h3) & (bitboard >> 3 * h3)) 
+				return true;
+			if (bitboard & (bitboard >> h4) & (bitboard >> 2 * h4) & (bitboard >> 3 * h4))
+				return true;
+
+			return false;
 		}
 	}
 }

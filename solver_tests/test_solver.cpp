10bfabd85507af5d8953716f1b62333c73cab0f7
Alex Cross <alexjcross90@gmail.com>
2023-02-19 22:37:33 +0000

    Minimax tests - seems to be working

--- solver_tests/test_solver.cpp
+++ solver_tests/test_solver.cpp
@@ -43,5 +43,45 @@ TEST(TestSolver, Minimax2) {
 	EXPECT_EQ(expected, actual);
 }
 
+TEST(TestSolver, Minimax3) {
+
+	// Arrange
+	Solver sut;
+
+	std::string moves_str = "245436534133154164526212";
+	std::vector<col_t> moves;
+	for (const char move : moves_str)
+		moves.push_back(move - '1');
+
+	int expected = -2;
+	Board board(moves);
+
+	// Act
+	auto actual = sut.minimax(board);
+
+	// Assert
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(TestSolver, Minimax4) {
+
+	// Arrange
+	Solver sut;
+
+	std::string moves_str = "512766432311321547131";
+	std::vector<col_t> moves;
+	for (const char move : moves_str)
+		moves.push_back(move - '1');
+
+	int expected = 2;
+	Board board(moves);
+
+	// Act
+	auto actual = sut.minimax(board);
+
+	// Assert
+	EXPECT_EQ(expected, actual);
+}
+
 
 
